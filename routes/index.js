44a621d7ecca31810fec6b125f14c9d7e18a92c9
Garrett Moore <gmoore10@fullsail.edu>
2018-06-27 20:41:16 -0400

    Form Submit Works. HTML Code Returned.

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,28 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/', function(req, res, next) {
+
+  let elements = req.body['elements[]'];
+
+  let returnedString = "";
+
+  elements.forEach(element => {
+    if(element === "text") {
+      returnedString = returnedString + '<input type="text" placeholder="Text Element" />'
+    }
+
+    if(element === "select") {
+      returnedString = returnedString + '<select><option>Option 1</option><option>Option 2</option><option>Option 3</option>'
+    }
+
+    if(element === "label") {
+      returnedString = returnedString + '<label for="yourElementName">Label: </label>'
+    }
+  });
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end(returnedString);
+  
+})
+
 module.exports = router;
